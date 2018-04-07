c60ae7f81c075c1da49471abdc9fde4dd3511238
Aditya Ramesh <ramesh.aditya@gmail.com>
2018-04-07 17:16:17 +0530

    [working] clean up

--- test_scheduler.py
+++ test_scheduler.py
@@ -4,20 +4,19 @@ from slackclient import SlackClient
 import random
 import xkcd
 import datetime
-#ob = xkcd.Comic(5)
 
-h=[]
 def slack_message():
-    global h
-
-    token = ''
+    
+    token = 'YOUR_LEGACY_TOKEN'
     sc = SlackClient(token)
 
-    history = sc.api_call("channels.history", channel = "CA2R3V1L5")
-    #print(history)
-    h = history
+    #acquire past messages on desired channel
+    history = sc.api_call("channels.history", channel = "CA2R3V1L5") #channel = "YOUR_CHANNEL_ID" (code to find, available below while loop)
+    
     command = history['messages'][0]['text']
-    if 'moar' in command:
+    if 'moar' in command:   #if user wants more comics
+        
+        #account for constantly increasing size of XKCD comics, and accordingly find random comic
         d1 = datetime.date(2012, 11, 6)
         now = datetime.datetime.now()
         d0 = datetime.date(now.year, now.month, now.day)
@@ -28,24 +27,23 @@ def slack_message():
         img_chosen = xkcd.Comic(rno)
         path = img_chosen.getImageLink()
     
+        #push message to channel
         sc.api_call('chat.postMessage', channel = 'general',
                     text = path, username='XKCDbot',
                     icon_emoji = ':robot_face:')
-        return 1
+        return 1 #success!
     return 0
 
-
-#def task():
-#    print(1)
-    
+#instantiate scheduler with task and rate
 schedule.every(5).seconds.do(slack_message)
 
+#execute task
 while True:
     schedule.run_pending()
     time.sleep(1)
 
 
-# Get Channel IDs using this
+# Get Channel IDs using this snippet
     #trial = sc.api_call("channels.list")
     #trialchannels = trial['channels']
     #for i in trialchannels:
